6b06e8bfbc0ef8f8f1c98e8a667b7f347afe6ece
hochan222 <201502766@hufs.ac.kr>
2019-02-10 23:03:38 +0900

    Change : basic example => Homepage ( Navigation )

---
+++ todo-ninja/src/views/Projects.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="projects">
+    <h1 class="subheading grey--text">Projects</h1>
+    
+    <v-container class="my-5">
+        projects
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
