9bc8acf29150d55f411d1d3d2bd303eb54d436c4
hochan222 <201502766@hufs.ac.kr>
2019-02-15 14:22:50 +0900

    change: egas

---
+++ egas/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app class="deep-purple">
+
+    <router-view></router-view>
+    
+    <!-- <v-content></v-content> -->
+  </v-app>
+</template>
+
+<script>
+import Navi from './components/Navi'
+
+export default {
+  name: 'App',
+  components: {
+    Navi
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ egas/src/views/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+    <v-layout
+      align-center
+      column
+      justify-center
+      fill-height
+    >
+      <v-card class="px-5 deep-purple accent-2">
+        <v-layout
+           justify-space-around
+         >
+          <v-avatar size="70" class="mt-4">
+            <img
+              src="https://cdn.vuetifyjs.com/images/john.jpg"
+              alt="John"
+            >
+          </v-avatar>
+        </v-layout>
+
+        <v-layout justify-center column align-center>
+          <v-card-title primary-title>
+            <div>
+              <p class="blue--text text--lighten-3 caption">___ invited you to join</p>
+              <h3 class="headline mb-0 mx-5 subheading deep-purple--text text--darken-4">Egas</h3>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn outline color="deep-purple darken-4"><router-link :to="'chat'"><span>Join</span></router-link></v-btn>
+          </v-card-actions>
+        </v-layout>
+      </v-card>
+    </v-layout>
+ </v-container>
+</template>
+
+
+<script>
+// import LoginF from '../components/Login-form'
+
+export default {
+  components: {
+    
+  }  
+}
+</script>
+
+<style>
+  a{
+    color: #311B92;
+    text-decoration: none;
+  }
+</style>
