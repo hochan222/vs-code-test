35d598c341f63e2cec508ee8fa6f5a26db75a086
egas <201502766@hufs.ac.kr>
2019-02-17 23:10:15 +0900

    express server

---
+++ nemv/nemv_express/routes/set.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '나는 셋이빈다' });
+});
+
+module.exports = router;
