98e155ad390934e46a15341a297e13fef1634656
egas <201502766@hufs.ac.kr>
2019-02-27 19:06:24 +0900

    react-native edit

--- nomad/react/ToDo/ToDo.js
+++ nomad/react/ToDo/ToDo.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from "react-native";
 
 const { width, height } = Dimensions.get("window");
 
 export default class ToDo extends Component{
     state = {
         isEditing: false,
-        isCompleted: false
+        isCompleted: false,
+        toDoValue: ""
     }
     render(){
-        const { isCompleted } = this.state;
+        const { isCompleted, isEditing, toDoValue } = this.state;
+        const { text } = this.props;
         return (
             <View style={styles.container}>
-                <TouchableOpacity onPress={this._togglecomplete}>
-                    <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
-                </TouchableOpacity>
-                <Text style={[styles.text, isCompleted ? styles.completedText : styles.uncompletedText]}>Hello I'm a To Do</Text>
+                <View style={styles.column}>
+                    <TouchableOpacity onPress={this._togglecomplete}>
+                        <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
+                    </TouchableOpacity>
+                    {isEditing ? (
+                        <TextInput style={[styles.text, styles.input, isCompleted ? styles.completedText : styles.uncompletedText] } value={toDoValue} multiline={true} onChangeText={this._controllInput} returnKeyType={"done"} onBlur={this._finishEditing}></TextInput>
+                    ) : (
+                        <Text style={[styles.text, isCompleted ? styles.completedText : styles.uncompletedText]}>{text}</Text>
+                    )
+                    }
+                </View>
+
+                    {isEditing ? (
+                        <View style={styles.actions}>
+                            <TouchableOpacity onPressOut={this._finishEditing}>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>✅</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    ) : (
+                        <View style={styles.actions}>
+                            <TouchableOpacity onPressOut={this._startEditing}>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>✏️</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>❌</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )}
             </View>
         )
     }
@@ -26,6 +58,23 @@ export default class ToDo extends Component{
             })
         })
     }
+    _startEditing = () => {
+        const { text } = this.props;
+        this.setState({
+            isEditing: true,
+            toDoValue: text
+        })
+    }
+    _finishEditing = () => {
+        this.setState({
+            isEditing: false
+        })
+    }
+    _controllInput = (text) => {
+        this.setState({
+            toDoValue: text
+        })
+    }
 }
 
 const styles = StyleSheet.create({
@@ -34,7 +83,8 @@ const styles = StyleSheet.create({
         borderBottomColor: "#bbb",
         borderBottomWidth: StyleSheet.hairlineWidth,
         flexDirection: "row",
-        alignItems: "center"
+        alignItems: "center",
+        justifyContent: "space-between"
     },
     circle: {
         width: 30,
@@ -53,5 +103,29 @@ const styles = StyleSheet.create({
     },
     uncompletedCircle: {
         borderColor: "#F23657"
+    },
+    completedText: {
+        color: "#bbb",
+        textDecorationLine : "line-through"
+    },
+    uncompletedText: {
+        color:"#353535"
+    },
+    column: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: width/2,
+        justifyContent: "space-between"
+    },
+    actions: {
+        flexDirection: "row"
+    },
+    actionContainer: {
+        marginVertical: 10,
+        marginHorizontal: 10
+    },
+    input: {
+        marginVertical: 15,
+        width: width/2
     }
 })
--- nomad/react/movieql-client/src/Home.js
+++ nomad/react/movieql-client/src/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+import {Query} from "react-apollo"
+import {HOME_PAGE} from "./queries.js"
 
-const Home = () => "Home"
+const Home = () => <Query query={HOME_PAGE}>{ ({loading,data,error}) => {
+    if(loading) return 'loading';
+    if(error) return 'something happen';
+    if(data) {
+        return data.movies.map( movie => (<h2 key={movie.id}>{movie.rating} / {movie.title}</h2>));
+    }
+}}</Query>
 
 export default Home;
